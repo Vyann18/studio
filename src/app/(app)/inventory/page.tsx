0d04431bd986b4a2a49347f78571899063c33c16
Achmad Vyan Krisnandha <vyan.krisna863@gmail.com>
2025-09-21 14:24:11 +0000

    Try fixing this error: `Runtime Error: Error: Card is not defined. Error

--- src/app/(app)/inventory/page.tsx
+++ src/app/(app)/inventory/page.tsx
@@ -46,6 +46,7 @@ import * as z from 'zod';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Badge } from '@/components/ui/badge';
+import { Card, CardContent } from '@/components/ui/card';
 import { inventoryItems } from '@/lib/data';
 import type { InventoryItem, Category } from '@/lib/types';
 import { MoreHorizontal, PlusCircle, Search } from 'lucide-react';
